99d6acc5ef0acb542dd7c2af85d117b5132ab1c7
YeonWoo Bae <82037372+sleepisparrow@users.noreply.github.com>
2025-12-14 20:03:22 +0900

    Title: 기수 정렬 구현, Time: 90ms, Memory: 13MB, Status: Wrong Answer - Codetree

---
+++ 251214/기수 정렬 구현/implement-radix-sort.js
@@ -0,0 +1,45 @@
+
+
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+let arr = input[1].split(' ').map(Number);
+
+// Please write your code here.
+
+// k 구하기
+let max = Math.max(...arr)
+let k = 0
+
+while (max >= 1) {
+    k++
+    max /= 10
+}
+
+// sort
+for(let target = 0; target < k; target++) {
+    // radixArr setting
+    const radixArr = []
+    for(let i = 0; i < 10; i++)
+        radixArr.push([])
+
+    // sort by ith number
+    for(let i = 0; i < n; i++) {
+        
+        let ith = Math.floor(arr[i] / Math.pow(10, target)) % 10
+        radixArr[ith].push(arr[i])
+    }
+
+    // make radixArr to 1 demension arr
+    let ret = []
+    for(let i = 0; i < 10; i++) {
+        for(let j = 0; j < radixArr[i].length; j++) {
+            ret.push(radixArr[i][j])
+        }
+    }
+
+    arr = ret
+}
+
+console.log(arr)
